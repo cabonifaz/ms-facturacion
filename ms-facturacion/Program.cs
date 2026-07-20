2a7c7339335b94be06f6e9e26898b5a3b4abb86f
Carlos <carlos.vasquez@fractalservicios.pe>
2026-07-20 11:22:26 -0500

    chore: agregar interfaz grafica de swagger

--- ms-facturacion/Program.cs
+++ ms-facturacion/Program.cs
@@ -12,6 +12,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "ms_facturacion API");
+    });
 }
 
 app.UseHttpsRedirection();
