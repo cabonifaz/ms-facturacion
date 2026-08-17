feb40ff5eb42488126f5c8e226eb3120cc36fd64
AshKuraine <lpc9720@hotmail.com>
2026-08-17 17:46:18 -0500

    feature: voiding manually on batchv6

--- ms-facturacion/Aplicacion/CasosDeUso/DocumentosElectronicos/GuardarCambiosDocumentoElectronicoCasoDeUso.cs
+++ ms-facturacion/Aplicacion/CasosDeUso/DocumentosElectronicos/GuardarCambiosDocumentoElectronicoCasoDeUso.cs
@@ -7,11 +7,11 @@ namespace ms_facturacion.Aplicacion.CasosDeUso.DocumentosElectronicos;
 public sealed class GuardarCambiosDocumentoElectronicoCasoDeUso(IDocumentoElectronicoRepositorio repositorio)
 {
     public Task<ResultadoOperacion<DocumentoElectronicoCambiosGuardados>> EjecutarAsync(
-        string usuarioEjecutor, int idInquilino, int idDocumentoElectronico, int? idFormaPago, string? numeroReferencia,
+        string usuarioEjecutor, int idInquilino, int idDocumentoElectronico, string idExterno, int? idFormaPago, string? numeroReferencia,
         int idMonedaMaestro, decimal? tipoCambio, int idTipoOperacionMaestro, int? idMotivoMaestro,
         IReadOnlyList<LineaDocumentoElectronicoEntrada> lineas, IReadOnlyList<CuotaDocumentoElectronicoEntrada> cuotas,
         IReadOnlyList<CampoExtraEntrada> camposExtra, CancellationToken cancellationToken) =>
         repositorio.GuardarCambiosAsync(
-            usuarioEjecutor, idInquilino, idDocumentoElectronico, idFormaPago, numeroReferencia,
+            usuarioEjecutor, idInquilino, idDocumentoElectronico, idExterno, idFormaPago, numeroReferencia,
             idMonedaMaestro, tipoCambio, idTipoOperacionMaestro, idMotivoMaestro, lineas, cuotas, camposExtra, cancellationToken);
 }
